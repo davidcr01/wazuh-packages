ec987d2fa7b60d6c68946273f9b4f4470545982c
davidcr01 <davidcorreafjb2001@gmail.com>
2023-01-05 09:22:11 +0100

    Testing braces in all code

--- .github/actions/upgrade-indexer/common.sh
+++ .github/actions/upgrade-indexer/common.sh
@@ -3,20 +3,11 @@ FILES_OLD="/usr/share/wazuh-indexer/plugins/opensearch-security/securityconfig"
 FILES_NEW="/etc/wazuh-indexer/opensearch-security"
 declare -A files_old
 declare -A files_new
-PACKAGE_NAME=$1
+PACKAGE_NAME="$1"
+MAJOR_MINOR_RELEASE=$(($2))
+REFERENCE_VERSION=43
 
 EQUAL=true
-
-# Checks the version of Wazuh with 4.3 version, where path is different.
-#function check_version() {
-#    if [ $WAZUH_VERSION -gt $REFERENCE_VERSION ]; then
-#        # same path
-#        FILES_OLD=$FILES_NEW
-#        echo "New path detected (/etc)"
-#    else
-#        echo "Old path detected (/usr/share)"
-#    fi
-#}
 
 # Check the system to differ between DEB and RPM
 function check_system() {
@@ -25,8 +16,26 @@ function check_system() {
         sys_type="rpm"
     elif [ -n "$(command -v apt-get)" ]; then
         sys_type="deb"
+    else
+        echo "Error: could not detect the system."
+        exit 1
     fi
 
+}
+
+# Checks the version of Wazuh with 4.3 version, where path is different.
+function check_version() {
+    if [ -z "${MAJOR_MINOR_RELEASE}" ]; then
+        echo "Error: second argument expected"
+        exit 1
+    fi
+
+    if [ "${MAJOR_MINOR_RELEASE}" -gt "${REFERENCE_VERSION}" ]; then
+        FILES_OLD="${FILES_NEW}"
+        echo "New path detected (/etc)."
+    else
+        echo "Old path detected (/usr/share)."
+    fi
 }
 
 # Compare the arrays, the loop ends if a different checksum is detected
@@ -37,16 +46,16 @@ function compare_arrays() {
         echo "Old: ${files_old[$i]}"
         echo "New: ${files_new[$i]}"
         if [[ "${files_old[$i]}" == "${files_new[$i]}" ]]; then
-            echo "$i - Same checksum"
+            echo "$i - Same checksum."
         else
-            echo "$i - Different checksum"
+            echo "$i - Different checksum."
             EQUAL=false
             break
         fi
     done
 }
 
-# Steps before installing the RPM release package
+# Steps before installing the RPM release package.
 function preinstall_indexer_release() {
     rpm --import https://packages.wazuh.com/key/GPG-KEY-WAZUH
     echo -e '[wazuh]\ngpgcheck=1\ngpgkey=https://packages.wazuh.com/key/GPG-KEY-WAZUH\nenabled=1\nname=EL-$releasever - Wazuh\nbaseurl=https://packages.wazuh.com/4.x/yum/\nprotect=1' | tee /etc/yum.repos.d/wazuh.repo
@@ -55,22 +64,27 @@ function preinstall_indexer_release() {
 # Reads the files passed by param and store their checksum in the array
 function read_files() {
 
+    if [ ! -d "${1}" ]; then
+        echo "Error: the directory does not exist. $1."
+        exit 1
+    fi
+
     for f in ${1}/*; do
-        if [ -f $f ]; then
+        if [ -f "${f}" ]; then
             echo "Processing $f file..."
 
             # Change only the old files
-            if [ $2 == "old" ]; then
+            if [ "${2}" == "old" ]; then
                 echo "# This is a test" >> $f
-                echo "Changed file"
+                echo "Changed file."
             fi
             checksum=`md5sum $f | cut -d " " -f1`
 
             basename=`basename $f`
-            if [ $2 == "old" ]; then
-                files_old["$basename"]=$checksum
-            elif [ $2 == "new" ]; then
-                files_new["$basename"]=$checksum
+            if [ "$2" == "old" ]; then
+                files_old["$basename"]="$checksum"
+            elif [ "$2" == "new" ]; then
+                files_new["$basename"]="$checksum"
             fi
         fi
     done
@@ -78,19 +92,24 @@ function read_files() {
 
 # Prints associative array of the files passed by params
 function print_files() {
+    if [ "${1}" == "old" ]; then
+        if [ "${#files_old[@]}" -eq 0 ]; then
+            echo "Error: the old version didn't scan correctly."
+            exit 1
+        fi
 
-    if [ $1 == "old" ]; then
         for KEY in "${!files_old[@]}"; do
-            # Print the KEY value
             echo "Key: $KEY"
-            # Print the VALUE attached to that KEY
             echo "Value: ${files_old[$KEY]}"
         done
-    elif [ $1 == "new" ]; then
+    elif [ "${1}" == "new" ]; then
+        if [ "${#files_new[@]}" -eq 0 ]; then
+            echo "Error: the new version didn't scan correctly."
+            exit 1
+        fi
+
         for KEY in "${!files_new[@]}"; do
-            # Print the KEY value
             echo "Key: $KEY"
-            # Print the VALUE attached to that KEY
             echo "Value: ${files_new[$KEY]}"
         done
     fi
