716ecbee008cc49edaf574860786dfb48e7c2381
davidcr01 <davidcorreafjb2001@gmail.com>
2023-01-12 12:17:38 +0100

    Checking error if different checksum detected

--- .github/actions/upgrade-indexer/common.sh
+++ .github/actions/upgrade-indexer/common.sh
@@ -81,10 +81,10 @@ function read_files() {
             echo "Processing ${f} file..."
 
             # Change only the old files
-            if [ "${2}" == "old" ]; then
+
                 echo "# This is a test" >> ${f}
                 echo "Changed file."
-            fi
+
             checksum=`md5sum ${f} | cut -d " " -f1`
 
             basename=`basename ${f}`
