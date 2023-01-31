c6237bbc7ec822818c5b3d27f644d5826ad3fb94
davidcr01 <davidcorreafjb2001@gmail.com>
2023-01-31 13:04:26 +0100

    Added workflow and action

---
+++ .github/actions/offline-installation/offline-installation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Gets the absolute path of the script, used to load the common.sh file
+ABSOLUTE_PATH="$( cd $(dirname ${0}) ; pwd -P )"
+. ${ABSOLUTE_PATH}/common.sh
+
+check_system
+download_packages
+
+indexer_installation
+echo "Wazuh indexer installation completed."
+
+manager_installation
+echo "Wazuh manager installation completed."
+
+filebeat_installation
+echo "Filebeat installation completed."
+
+dashboard_installation
+echo "Wazuh dashboard installation completed."
+
