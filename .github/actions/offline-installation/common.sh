0df5b4162ca33f47dc8e33656ed156f0f8670889
davidcr01 <davidcorreafjb2001@gmail.com>
2023-02-02 13:47:21 +0100

    Dependencies changed, starting services manually in RPM

--- .github/actions/offline-installation/common.sh
+++ .github/actions/offline-installation/common.sh
@@ -161,7 +161,7 @@ function indexer_installation(){
 
     if [ "${sys_type}" == "rpm" ]; then
         runuser "wazuh-indexer" --shell="/bin/bash" --command="OPENSEARCH_PATH_CONF=/etc/wazuh-indexer /usr/share/wazuh-indexer/bin/opensearch" > /dev/null 1>&2 &
-        sleep 10
+        sleep 20
     elif [ "${sys_type}" == "deb" ]; then
         enable_start_service "wazuh-indexer"
     fi
